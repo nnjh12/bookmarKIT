14188c2a6e1fdeaeb96e0b7a8067756247771b16
Jinhwa Roh <nnjh12@gmail.com>
2019-11-11 23:34:07 -0500

    set up folders and files

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import InputPage from "./components/InputPage";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+      <InputPage></InputPage>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/InputPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { MDBInput } from "mdbreact";
+
+const InputPage = () => {
+    return (
+        <>
+            <MDBInput type="textarea" label="Example label" outline />
+        </>
+    );
+}
+
+export default InputPage;
