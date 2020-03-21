833fae168782455a0bfbd86e9563871545dbb512
Gina <nnjh12@gmail.com>
2020-03-21 17:03:44 -0400

    styling tag button

--- client/src/components/TagButton.js
+++ client/src/components/TagButton.js
@@ -7,14 +7,23 @@ import Highlight from "./Highlight";
 class TagButton extends Component {
     render() {
         return (
-            <Button className="float-left" color={this.props.search.includes(this.props.text) ? "warning" : "default" }>
-                <div>
-                    <div className="float-right ml-3">
-                        <DeleteIcon fontColor="white" deleteOnClick={this.props.deleteTag}></DeleteIcon>
-                    </div>
-                    <span>#</span><Highlight text={this.props.text} highlight={this.props.highlight}></Highlight>
-                </div>
-            </Button>
+            <div
+                className="tagButton"
+                style={{
+                    backgroundColor: this.props.search.includes(this.props.text) ? "#f96738" : "",
+                    color: this.props.search.includes(this.props.text) ? "#ffffff" : "",
+                    borderColor: this.props.search.includes(this.props.text) ? "#f96738" : "",
+                }}>
+                <span>#</span><Highlight text={this.props.text} highlight={this.props.highlight}></Highlight>
+            </div>
+            // <Button className="float-left" color={this.props.search.includes(this.props.text) ? "warning" : "default" }>
+            //     <div>
+            //         <div className="float-right ml-3">
+            //             <DeleteIcon fontColor="white" deleteOnClick={this.props.deleteTag}></DeleteIcon>
+            //         </div>
+            //         <span>#</span><Highlight text={this.props.text} highlight={this.props.highlight}></Highlight>
+            //     </div>
+            // </Button>
         );
     }
 }
