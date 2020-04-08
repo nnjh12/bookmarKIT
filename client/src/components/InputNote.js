7899b2a7fbd8379e1131bd412d0e8fabaa41a037
Gina <nnjh12@gmail.com>
2020-04-08 15:24:29 -0400

    before clean code

--- client/src/components/InputNote.js
+++ client/src/components/InputNote.js
@@ -7,11 +7,9 @@ class InputNote extends Component {
         this.state = {
             bookmarkUserInput: "",
             keywordUserInput: "",
-            tagUserInput: "",
             placeholder: {
                 bookmark: "Add your bookmark here.",
                 keyword: "Add your keyword here.",
-                tag: "#my tag #example #hello"
             }
         };
     }
@@ -23,9 +21,6 @@ class InputNote extends Component {
     };
     onSubmit = (newTag) => {
         // event.preventDefault()
-        // const tagArray = this.state.tagUserInput.split("#").map(item => item.trim());
-        // const filtered = tagArray.filter(item => item);
-
         const newNote = {
             bookmark: this.state.bookmarkUserInput,
             keyword: this.state.keywordUserInput,
@@ -35,7 +30,6 @@ class InputNote extends Component {
         this.setState({
             bookmarkUserInput: "",
             keywordUserInput: "",
-            tagUserInput: ""
         });
     }
     render() {
@@ -61,15 +55,6 @@ class InputNote extends Component {
                     placeholder={this.state.placeholder.keyword}>
                 </input>
                 <label htmlFor="tag">TAG</label>
-                {/* <input
-                    type="text"
-                    id="tag"
-                    name="tagUserInput"
-                    style={{ width: "100%" }}
-                    onChange={this.handleInputChange}
-                    value={this.state.tagUserInput}
-                    placeholder={this.state.placeholder.tag}>
-                </input> */}
                 <TagInput
                     inputId="tag"
                     userAllTag={this.props.userAllTag}
@@ -77,12 +62,6 @@ class InputNote extends Component {
                     disabled={this.state.bookmarkUserInput}
                     close={this.props.close}>
                 </TagInput>
-
-                {/* <input type="submit" value="Save" disabled={this.state.bookmarkUserInput ? false : true} onClick={this.onSubmit}></input> */}
-
-                {/* <TextInput name="newNote" row="5" onChange={this.handleInputChange} value={this.state.newNote} placeholder={this.state.placeholder.note}>Note</TextInput>
-                <TextInput name="newTag" row="2" onChange={this.handleInputChange} value={this.state.newTag} placeholder={this.state.placeholder.tag}>Tag</TextInput>
-                <button disabled={this.state.newNote ? false : true} onClick={this.onSubmit}>save</button> */}
             </form>
         );
     }
